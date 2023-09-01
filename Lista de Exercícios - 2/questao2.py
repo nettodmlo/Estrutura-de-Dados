db6005c5cbda154a9b0c7dd28abf1ebaccf7cedd
Antônio Venâncio <114105731+nettodmlo@users.noreply.github.com>
2023-09-01 19:41:43 -0300

    Delete Lista de Exercícios - 2 directory

--- Lista de Exercícios - 2/questao2.py
+++
@@ -1,9 +0,0 @@
-# 2. Crie uma classe chamada “Livro” que tenha atributos “titulo” e “autor”. Implemente um método 
-# chamado “detalhes” que retorna uma string com as informações do livro.
-
-class Livro:
-    def detalhes(titulo, autor):
-        print(f"Livro com titulo de {titulo}, escritor por {autor}")
-
-livro1 = Livro
-livro1.detalhes("E o vento levou", "Paulo Renan")
--- Lista de Exercícios - 2/questao7.py
+++
@@ -1,9 +0,0 @@
-# 7. Crie uma classe chamada “Carro” com atributos “marca”, “modelo” e “ano”. Implemente um método 
-# chamado “detalhes” que retorna uma string com as informações do carro
-
-class Carro:
-    def detalhes(self, marca, modelo, ano):
-        print(f"O carro é do modelo {modelo} da {marca}, ano {ano}")
-
-carro1 = Carro()
-carro1.detalhes("Renault", "Kwid", 2020)
